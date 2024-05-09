e125416065d7595aa20c5e7c5559841dce278558
Matt Savoia <matt@1-800-rad-dude.com>
2024-05-08 23:11:32 -0400

    Add PresentationServer

---
+++ lib/live_slides/presentations/presentation_server.ex
@@ -0,0 +1,43 @@
+defmodule LiveSlides.Presentations.PresentationServer do
+  @moduledoc """
+  Client/Server module to centralize state of a presentation.
+  """
+
+  use GenServer
+
+  alias LiveSlides.Presentations.{Deck, PresentationState}
+
+  @doc """
+  Starts a PresentationServer process linked to the current process.
+  """
+  def start_link({id, %Deck{} = deck}) do
+    GenServer.start_link(__MODULE__, deck, name: name(id))
+  end
+
+  @doc """
+  Gets the PresentationServer name from an id. Can be used to send messages to
+  the PresentationServer.
+  """
+  def name(id) do
+    {:global, global_id(id)}
+  end
+
+  defp global_id(id) do
+    {:presentation, id}
+  end
+
+  @doc """
+  Returns true if a PresentationServer process exists for this id.
+  """
+  def exists?(id) do
+    case :global.whereis_name(global_id(id)) do
+      :undefined -> false
+      pid when is_pid(pid) -> true
+    end
+  end
+
+  @impl true
+  def init(deck) do
+    {:ok, PresentationState.new(deck)}
+  end
+end
---
+++ test/live_slides/presentations/presentation_server_test.exs
@@ -0,0 +1,20 @@
+defmodule LiveSlides.Presentations.PresentationServerTest do
+  use LiveSlides.DataCase
+
+  import LiveSlides.PresentationsFixtures
+
+  alias LiveSlides.Presentations.PresentationServer
+
+  test "integration test" do
+    deck = deck_fixture()
+    id = Ecto.UUID.generate()
+
+    other_deck = deck_fixture()
+    other_id = Ecto.UUID.generate()
+
+    refute PresentationServer.exists?(id)
+    start_supervised!({PresentationServer, {id, deck}}, id: id)
+    assert PresentationServer.exists?(id)
+    start_supervised!({PresentationServer, {other_id, other_deck}}, id: other_id)
+  end
+end
