94f5eaf8fb163c1863971e2781efc8f5dc69c65d
Matt Savoia <matt@1-800-rad-dude.com>
2024-05-08 22:41:42 -0400

    Add repo seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias LiveSlides.Accounts
+alias LiveSlides.Presentations
+
+{:ok, user} =
+  Accounts.register_user(%{email: "matt@1-800-rad-dude.com", password: "KS4E8zTH64283v"})
+
+{:ok, _deck} =
+  Presentations.create_deck(%{
+    title: "Test Deck 1",
+    user_id: user.id,
+    slides: [
+      %{body: "first slide"},
+      %{body: "second slide"},
+      %{body: "third slide"},
+      %{body: "fourth slide"},
+      %{body: "fifth slide"}
+    ]
+  })
