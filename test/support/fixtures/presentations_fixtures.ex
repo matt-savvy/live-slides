0dcca7a0e5a303da68497031d51a058f2034ab0b
Matt Savoia <matt@1-800-rad-dude.com>
2024-05-09 00:11:57 -0400

    Update deck_fixture, add user/slides

--- test/support/fixtures/presentations_fixtures.ex
+++ test/support/fixtures/presentations_fixtures.ex
@@ -4,14 +4,29 @@ defmodule LiveSlides.PresentationsFixtures do
   entities via the `LiveSlides.Presentations` context.
   """
 
+  import LiveSlides.AccountsFixtures
+
   @doc """
   Generate a deck.
   """
   def deck_fixture(attrs \\ %{}) do
+    user_id =
+      Map.get_lazy(attrs, :user, fn ->
+        %{id: user_id} = user_fixture()
+
+        user_id
+      end)
+
     {:ok, deck} =
       attrs
       |> Enum.into(%{
-        title: "some title"
+        user_id: user_id,
+        title: "some title",
+        slides: [
+          %{body: "this is the first slide"},
+          %{body: "this is the second slide"},
+          %{body: "this is the third slide"}
+        ]
       })
       |> LiveSlides.Presentations.create_deck()
 
